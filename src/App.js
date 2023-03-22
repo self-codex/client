f18b60131e9b2e8e5a534bcd1411b36cb21204be
Muhammad Talha <mtalha0018@gmail.com>
2023-03-22 19:51:02 +0500

    c

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/header/Header";
 import Home from "./components/Home";
 import Resume from "./components/resume/Resume";
 
+//
 function App() {
   return (
     <div className="App">
